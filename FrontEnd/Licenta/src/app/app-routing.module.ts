a4ef9edcbf0e1745c05b6d3f51fa9e2d5d95b593
DESKTOP-NF9G3TJ\40743 <coralia_ics3@yahoo.com>
2023-09-30 19:41:45 +0300

    login component

--- FrontEnd/Licenta/src/app/app-routing.module.ts
+++ FrontEnd/Licenta/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CalendarComponent } from './schedule/calendar/calendar.component';
 import { ProfilePageComponent } from './home/profile-page/profile-page.component';
+import { LoginComponent } from './login/login/login.component';
 
 const routes: Routes = [
   { path: 'profile', component: ProfilePageComponent },
-  { path: 'schedule', component: CalendarComponent }
+  { path: 'schedule', component: CalendarComponent },
+  { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
